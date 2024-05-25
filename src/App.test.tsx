902255129b87710f4a3e0ee1fd5b2773bd8484c0
Stanislau Isayeu <s.isaev@itransition.com>
2024-05-25 15:29:51 +0300

    Fixed tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { ALL_COURSES_TEXT } from './modules/courses/courses.constants';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(ALL_COURSES_TEXT, {exact: false});
   expect(linkElement).toBeInTheDocument();
 });
